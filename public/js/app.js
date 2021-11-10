f5c5892dbf6a49c1ec34bfc920763c95e13b98c1
jihami <w2027@e-mirim.hs.kr>
2021-11-11 03:24:55 +0900

    DB Example

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA6LFw48XiIdVdfZxP34Xmr0QFifUlO4Ik",
+    authDomain: "ttst-1f284.firebaseapp.com",
+    databaseURL: "https://ttst-1f284-default-rtdb.firebaseio.com",
+    projectId: "ttst-1f284",
+    storageBucket: "ttst-1f284.appspot.com",
+    messagingSenderId: "907858494375",
+    appId: "1:907858494375:web:e8d519daa827cd6a076c82",
+    measurementId: "G-BZ7Q1188LB"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
