a6b6c720e20d0bcc4c105bdfb39f49b8a2ddc8e2
jihami <w2027@e-mirim.hs.kr>
2021-11-19 09:59:31 +0900

    database 가져오기

--- public/js/reviewList.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('hi').innerHTML="이순신";
-var dbTestRef = database.ref('wlgka/')
-dbTestRef.on('child_added', function(data){
-console.log(data.val())
-document.getElementById('hi').innerHTML=data.val();
-});
